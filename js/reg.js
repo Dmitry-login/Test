c53ec6a5c67cfc03ef2b8f8bc2aeef687bf215de
Dmitry-login <71512791+Dmitry-login@users.noreply.github.com>
2020-09-18 18:05:57 +0300

    first commit

---
+++ js/reg.js
@@ -0,0 +1,8 @@
+login = function() {
+		document.getElementById("hide").style.display = "block";
+  		document.getElementById("window").style.display = "block";
+		}
+hide = function() {
+		document.getElementById("window").style.display = "none";
+  		document.getElementById("hide").style.display = "none";	
+		}
